ac74526af27a376eb128c6bb2da085d4b195c3f3
carsongmiller <carsongmiller@gmail.com>
2023-01-13 09:51:51 -0600

    Added assembly properties

--- ScreenShotter/Properties/AssemblyInfo.cs
+++ ScreenShotter/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ScreenShotter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Takes periodic screenshots, plus some other useful utilities")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Razer")]
-[assembly: AssemblyProduct("ScreenShotter")]
-[assembly: AssemblyCopyright("Copyright © Razer 2023")]
+[assembly: AssemblyCompany("Carson Miller")]
+[assembly: AssemblyProduct("ScreenShotter 3000X")]
+[assembly: AssemblyCopyright("Carson Miller")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
